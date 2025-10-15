4bc1fbcb14ccc50a226439209b207597777fa040
Jelena Vasić <jelena.vasic@tudublin.ie>
2025-10-15 09:21:49 +0100

    deploy module notes

---
+++ images/rwat/jsbrws/1_supporting/page.js
@@ -0,0 +1,9 @@
+
+function changePage () {
+    document.getElementById("pgtt").innerHTML = "TU Dublin Wallpaper"
+    document.getElementsByTagName("p")[0].style.color = "red"
+}
+
+console.log("Function changePage has been defined.")
+document.getElementById("pgtt").insertAdjacentText("beforeend", " So Blue")
+
